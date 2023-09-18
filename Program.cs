c69ca88fbde7c1c6c352f2746da3d87cbba88ff1
Heather Wolf <Z1893923@students.niu.edu>
2023-09-18 11:26:25 -0500

    initial Commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello MEE 381");
+
+        float a = 0.72f;
+        float b = 0.33f;
+        float c = 1.05f;
+
+        if(a+b == c){
+            Console.WriteLine("They're equal");
+        }
+        else{
+            Console.WriteLine("not equal");
+        }
+
+        Console.WriteLine("a {0:G6}",a);
+    }
+}
